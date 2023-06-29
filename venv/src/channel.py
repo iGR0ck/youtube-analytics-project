7603c23cff77fc266e7a2bf917ba09eeabbd4b14
igorpetushkov <igor.igorevich08@gmail.com>
2023-06-29 04:50:35 +0300

    дз по тз

--- venv/src/channel.py
+++ venv/src/channel.py
@@ -1,8 +1,15 @@
-from venv.helper.youtube_api_manual import youtube
+from googleapiclient.discovery import build
+import os
 import json
 
 class Channel:
     """Класс для ютуб-канала"""
+
+    # YT_API_KEY скопирован из гугла и вставлен в переменные окружения
+    api_key: str = os.getenv('YT_API_KEY')
+
+    # создать специальный объект для работы с API
+    youtube = build('youtube', 'v3', developerKey=api_key)
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
@@ -10,8 +17,6 @@ class Channel:
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        channel = Channel.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
         info = json.dumps(channel, indent=2, ensure_ascii=False)
-        print(info)
-        print(self.channel_id)
-        return info
+        print (info)
