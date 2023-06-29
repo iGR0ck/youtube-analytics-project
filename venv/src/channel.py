948ee40990462e7ce461f2af0be4e7ab3a827f6a
igorpetushkov <igor.igorevich08@gmail.com>
2023-06-29 04:39:00 +0300

    настроена переменная окружения

---
+++ venv/src/channel.py
@@ -0,0 +1,17 @@
+from venv.helper.youtube_api_manual import youtube
+import json
+
+class Channel:
+    """Класс для ютуб-канала"""
+
+    def __init__(self, channel_id: str) -> None:
+        """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
+        self.channel_id = channel_id
+
+    def print_info(self) -> None:
+        """Выводит в консоль информацию о канале."""
+        channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        info = json.dumps(channel, indent=2, ensure_ascii=False)
+        print(info)
+        print(self.channel_id)
+        return info
